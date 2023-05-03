6a67abf595330df14a52b9e378d2aee3e61ba921
Shahidul Islam <shahid.okay@gmail.com>
2023-05-03 21:41:23 +0000

    Added first component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1> Hello from my react</h1>
+// }
+
+
+const FunctionalGreeting = () => <h1>Hello from react</h1>
+
+export default FunctionalGreeting;
